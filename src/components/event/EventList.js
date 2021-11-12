be7bbd661e5f775987aa6cffb0c30f9cada6b4e0
Brandon De Leon <brandondeleon.615@gmail.com>
2021-11-12 15:25:55 -0600

    finished ch10

--- src/components/event/EventList.js
+++ src/components/event/EventList.js
@@ -1,25 +1,49 @@
 import React, { useEffect, useState } from "react"
-import { getEvents } from "./EventManager";
+import { getEvents, joinEvent } from "./EventManager";
+import { useHistory } from "react-router-dom";
 
 
 export const EventList = () => {
 
     const [events, setEvents] = useState([])
+    const history = useHistory()
+    
+
+    const eventFetcher = () => {
+        getEvents().then(data => setEvents(data))
+    }
 
     useEffect(() => {
-        getEvents().then(eventsData => setEvents(eventsData))
+        eventFetcher()
     }, [])
-
 
     return (
         <article className="events">
-            <h2>Current Events</h2>
+            <header className="events__header">
+                <h1>Level Up Game Events</h1>
+                <button className="btn btn-2 btn-sep icon-create"
+                    onClick={() => {
+                        history.push({ pathname: "/events/new" })
+                    }}
+                >Schedule New Event</button>
+            </header>
             {
                 events.map(event => {
                     return <section key={`event--${event.id}`} className="event">
+                    
+                        <div className="registration__game">{event.game.title}</div>
                         <p>Event Description: {event.description}</p>
-                        <p>Event Date: {event.date}</p>
-                        <p>Event Time: {event.time}</p>
+                        <div>
+                            {event.date} @ {event.time}
+                        </div>
+                        <button className="btn btn-2"
+                            onClick={
+                                () => {
+                                    joinEvent(event.id)
+                                        .then(eventFetcher)
+                                }
+                            }
+                        >Join</button>
                     </section>
                 })
             }
--- src/components/event/EventManager.js
+++ src/components/event/EventManager.js
@@ -21,3 +21,13 @@ export const createEvent = (event)=>{
     })
     
 }
+
+export const joinEvent = eventId => {
+    return fetch(`http://localhost:8000/events/${ eventId }/signup`, {
+        method: "POST",
+        headers:{
+            "Authorization": `Token ${localStorage.getItem("lu_token")}`
+        }
+    })
+        .then(response => response.json())
+}
